c0eb9cd2c47a746cdda4ea94a2ef3d2c64f5b527
Velaro <kamilmiem@gmail.com>
2019-03-22 23:49:04 +0300

    draft: Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
+import { FilmsListComponent } from './films/films-list/films-list.component';
+import { FilmDetailComponent } from './films/film-detail/film-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
+import { BannerComponent } from './films/banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilmsComponent,
+    FilmsListComponent,
+    FilmDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
